16765082e79b0eb997b0a30d2bfc409572df7066
Iain Dillingham <iain@dillingham.me.uk>
2021-04-16 11:35:24 +0100

    Add common_variables and refactor

--- analysis/rotate_study_population.py
+++ analysis/rotate_study_population.py
@@ -1,10 +1,9 @@
 import shutil
-from pathlib import Path
 
-BASE_DIR = Path("output")
+from common_variables import OUTPUT_DIR
 
 if __name__ == "__main__":
-    f_in = BASE_DIR / "input.feather"
-    f_out = BASE_DIR / "input-last.feather"
+    f_in = OUTPUT_DIR / "input.feather"
+    f_out = OUTPUT_DIR / "input-last.feather"
     print(f"Copying from {f_in} to {f_out}")
     shutil.copy(f_in, f_out)
