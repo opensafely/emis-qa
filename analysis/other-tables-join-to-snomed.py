3961a27654a29b9855b9edca8cb433616cf6c581
HelenCEBM <helencebm@gmail.com>
2021-06-01 10:14:13 +0100

    Comparisons and opencodelists
    - create a version of each codelist to load to opencodelists
    - compare overlap between codelists

--- analysis/other-tables-join-to-snomed.py
+++ analysis/other-tables-join-to-snomed.py
@@ -16,3 +16,10 @@ for t in tablenames:
     out_path = os.path.join("released-outputs", "other-tables")
     os.makedirs(out_path, exist_ok=True)
     out.to_csv(os.path.join(out_path, f"{t}.csv"), index=False)
+
+    if t != 'observation':
+    # export for opencodelists
+    # remove unnecessary columns, ensure description is not missing (because codelist can't be uploaded with unknown codes present), 
+    # and remove "Clinical findings" (this code has a lot of descendents hence takes a lot of processing in opencodelists)
+        out_codelist = out[['snomed_concept_id', 'description']].loc[pd.notnull(out["description"]) & (out["snomed_concept_id"]!=404684003)]
+        out_codelist.to_csv(os.path.join(out_path, f"{t}_codelist.csv"), index=False, header=False)
